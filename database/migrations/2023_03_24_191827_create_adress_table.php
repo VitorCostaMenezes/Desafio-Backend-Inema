f73154077706a121604eda6c855e796baba3f283
Vitor Costa <vitorcostinha@hotmail.com>
2023-03-24 16:45:21 -0300

    feat:contrucao das migrations

---
+++ database/migrations/2023_03_24_191827_create_adress_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdressTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('adress', function (Blueprint $table) {
+            $table->id();
+            $table->string('rua', 100);
+            $table->string('bairro', 50);
+            $table->string('cidade', 50);
+            $table->string('numero', 10);
+            $table->string('estado', 5);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('adress');
+    }
+}
